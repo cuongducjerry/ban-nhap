0fc1a207e8c081f407f19d7d38222ee9d78b1059
Nguyen Duc Cuong <cuongducjerry@gmail.com>
2025-02-07 10:29:56 +0700

    JAVABE2411 2

--- employee-manager/src/main/java/vn/edu/t3h/employeemanager/HelloServlet.java
+++ employee-manager/src/main/java/vn/edu/t3h/employeemanager/HelloServlet.java
@@ -5,7 +5,7 @@ import java.io.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-
+//hello-servlet
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
